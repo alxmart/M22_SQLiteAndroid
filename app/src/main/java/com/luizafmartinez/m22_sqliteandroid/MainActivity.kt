7bd2a8f33ef4a04814e0c5d7e708d405424717f8
alxmart <alxmartinez@gmail.com>
2024-05-07 15:19:17 -0300

    Changing code to use const.

--- app/src/main/java/com/luizafmartinez/m22_sqliteandroid/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m22_sqliteandroid/MainActivity.kt
@@ -2,10 +2,7 @@ package com.luizafmartinez.m22_sqliteandroid
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.luizafmartinez.m22_sqliteandroid.database.DatabaseHelper
 import com.luizafmartinez.m22_sqliteandroid.databinding.ActivityMainBinding
 
@@ -43,14 +40,19 @@ class MainActivity : AppCompatActivity() {
         val sql = "SELECT * FROM ${DatabaseHelper.TABELA_PRODUTOS};"
         val cursor = bancoDados.readableDatabase.rawQuery(sql, null)
 
-        while ( cursor.moveToNext() ) {  // false ou true
+        val indiceId = cursor.getColumnIndex("${DatabaseHelper.ID_PRODUTO}")
+        val indiceTitulo = cursor.getColumnIndex("${DatabaseHelper.TITULO}")
+        val indiceDescricao = cursor.getColumnIndex("${DatabaseHelper.DESCRICAO}")
 
-            val idProduto = cursor.getInt(0)
-            val titulo = cursor.getString(1)
-            val descricao = cursor.getString(2)
+        while (cursor.moveToNext()) {  // false ou true
+            val idProduto = cursor.getInt(indiceId)
+            val titulo = cursor.getString(indiceTitulo)
+            val descricao = cursor.getString(indiceDescricao)
 
-            Log.i("info_db",
-                  "id: $idProduto, titulo: $titulo, descricao: $descricao")
+            Log.i(
+                "info_db",
+                "id: $idProduto, titulo: $titulo, descricao: $descricao"
+            )
         }
     }
 
@@ -59,27 +61,16 @@ class MainActivity : AppCompatActivity() {
         salvar()
     }
 
-        private fun salvar() {
+    private fun salvar() {
 
-            val titulo = binding.editProduto.text.toString()
-            val sql = "INSERT INTO produtos VALUES(null, '$titulo', 'Descricao...');"
+        val titulo = binding.editProduto.text.toString()
+        val sql = "INSERT INTO produtos VALUES(null, '$titulo', 'Descricao...');"
 
-            try {
-                bancoDados.writableDatabase.execSQL( sql )
-                Log.i("info_db", "Sucesso ao inserir")
-            } catch (e: Exception) {
-                Log.i("info_db", "Erro ao inserir")
-            }
+        try {
+            bancoDados.writableDatabase.execSQL(sql)
+            Log.i("info_db", "Sucesso ao inserir")
+        } catch (e: Exception) {
+            Log.i("info_db", "Erro ao inserir")
         }
-
-
-
+    }
 }
-
-
-
-
-
-
-
-
